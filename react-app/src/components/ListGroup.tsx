69ad718697918c311bdb7d34be8608781dc80551
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-17 15:32:53 +0800

    update ListGroup

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,29 +1,27 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-  let items = ["Manila", "Paranaque", "Las Pinas", "Muntinlupa", "Taguig"];
-  items = [];
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-  }
+    let items = ["Manila", "Paranaque", "Las Pinas", "Muntinlupa", "Taguig"];
+    items = [];
 
-  return (
-    //Fragment for no div in DOM, div has DOM, <> for Fragment in react
-    <>
-      <h1>List</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          //key can be item.id
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </>
-  );
+    // const message = items.length === 0 ? <p>No Item Found</p> : null;
+    // const getMessage = () => {
+    //     return items.length === 0 ? <p>No Item Found</p> : null;
+    // };
+
+    return (
+        //Fragment for no div in DOM, div has DOM, <> for Fragment in react
+        <>
+            <h1>List</h1>
+            {items.length === 0 && <p>No Item Found</p>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    //key can be item.id
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default ListGroup;
