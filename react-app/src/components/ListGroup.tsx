8c8d04ea9cccb03ca1e2522a70f8613014cfdd28
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-17 15:43:20 +0800

    function MouseEvent

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 // import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
     let items = ["Manila", "Paranaque", "Las Pinas", "Muntinlupa", "Taguig"];
-    items = [];
+    // items = [];
 
     // const message = items.length === 0 ? <p>No Item Found</p> : null;
     // const getMessage = () => {
     //     return items.length === 0 ? <p>No Item Found</p> : null;
     // };
+
+    //Event Handler
+    const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
         //Fragment for no div in DOM, div has DOM, <> for Fragment in react
@@ -15,9 +19,16 @@ function ListGroup() {
             <h1>List</h1>
             {items.length === 0 && <p>No Item Found</p>}
             <ul className="list-group">
-                {items.map((item) => (
+                {items.map((item, index) => (
                     //key can be item.id
-                    <li key={item}>{item}</li>
+                    <li
+                        className="list-group-item"
+                        key={item}
+                        // onClick={() => console.log(item, index)}
+                        onClick={handleClick}
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
         </>
