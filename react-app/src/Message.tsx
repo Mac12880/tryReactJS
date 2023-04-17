43d8b3a6cbc3593413b172db30825783f8a7efaa
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-17 14:36:14 +0800

    Hello Marc

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,10 @@
 //PascalCasing to create a function
 function Message() {
+    const name = 'Marc';
+    if (name){
     //JSX JavaScript XML
+    return <h1>Hello {name}</h1>;
+    }
     return <h1>Hello World</h1>;
 }
 
